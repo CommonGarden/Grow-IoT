ce7b382dd09bb84daf401223172d3430a6d9283d
Jake Hartnell <jake.hartnell@gmail.com>
2016-06-08 14:09:30 -0700

    Chartist is cool.

--- packages/app/visualization/linechart.js
+++ packages/app/visualization/linechart.js
@@ -17,34 +17,107 @@ class LineChartComponent extends CommonComponent {
   onRendered() {
     super.onRendered();
 
-    // TODO: make this a bit more responsive? Perhaps get the width of the div and
-    // set the width to that?
-    let margin = { 
-      top: 20,
-      right: 20,
-      bottom: 30,
-      left: 50
+    let data = {
+      // A labels array that can contain any sort of values
+      labels: [],
+      // Our series array that contains series objects or in this case series data arrays
+      series: [[]]
     };
-    // TODO: make this a bit more reponsive for smaller devices?
-    let width = 600 - (margin.left) - (margin.right);
-    let height = 400 - (margin.top) - (margin.bottom);
-    let x = d3.time.scale().range([
-      0,
-      width
-    ]);
-    let y = d3.scale.linear().range([
-      height,
-      0
-    ]);
-    
-    // Here we namespace the visualization instance so there can be multiple visualizations on a page.
-    let instance = {};
-    instance[this.property()] = {};
-    instance[this.property()].xAxis = d3.svg.axis().scale(x).orient('bottom');
-    instance[this.property()].yAxis = d3.svg.axis().scale(y).orient('left');
-    instance[this.property()].svg = d3.select(`#${this.property()}`).attr('width', width + margin.left + margin.right).attr('height', height + margin.top + margin.bottom).append('g').attr('transform', `translate(${margin.left},${margin.top})`);
-    instance[this.property()].svg.append('g').attr('class', 'x axis').attr('transform', `translate(0,${height})`);
-    instance[this.property()].svg.append('g').attr('class', 'y axis').append('text').attr('transform', 'rotate(-90)').attr('y', 6).attr('dy', '.71em').style('text-anchor', 'end').text(''); // TODO: Add unit value if it exists
+
+    let options = {
+      // Options for X-Axis
+      axisX: {
+        // The offset of the labels to the chart area
+        offset: 30,
+        // Position where labels are placed. Can be set to `start` or `end` where `start` is equivalent to left or top on vertical axis and `end` is equivalent to right or bottom on horizontal axis.
+        position: 'end',
+        // Allows you to correct label positioning on this axis by positive or negative x and y offset.
+        labelOffset: {
+          x: 0,
+          y: 0
+        },
+        // If labels should be shown or not
+        showLabel: true,
+        // If the axis grid should be drawn or not
+        showGrid: true,
+        // Interpolation function that allows you to intercept the value from the axis label
+        labelInterpolationFnc: Chartist.noop,
+        // Set the axis type to be used to project values on this axis. If not defined, Chartist.StepAxis will be used for the X-Axis, where the ticks option will be set to the labels in the data and the stretch option will be set to the global fullWidth option. This type can be changed to any axis constructor available (e.g. Chartist.FixedScaleAxis), where all axis options should be present here.
+        type: undefined
+      },
+      // Options for Y-Axis
+      axisY: {
+        // The offset of the labels to the chart area
+        offset: 40,
+        // Position where labels are placed. Can be set to `start` or `end` where `start` is equivalent to left or top on vertical axis and `end` is equivalent to right or bottom on horizontal axis.
+        position: 'start',
+        // Allows you to correct label positioning on this axis by positive or negative x and y offset.
+        labelOffset: {
+          x: 0,
+          y: 0
+        },
+        // If labels should be shown or not
+        showLabel: true,
+        // If the axis grid should be drawn or not
+        showGrid: true,
+        // Interpolation function that allows you to intercept the value from the axis label
+        labelInterpolationFnc: Chartist.noop,
+        // Set the axis type to be used to project values on this axis. If not defined, Chartist.AutoScaleAxis will be used for the Y-Axis, where the high and low options will be set to the global high and low options. This type can be changed to any axis constructor available (e.g. Chartist.FixedScaleAxis), where all axis options should be present here.
+        type: undefined,
+        // This value specifies the minimum height in pixel of the scale steps
+        scaleMinSpace: 20,
+        // Use only integer values (whole numbers) for the scale steps
+        onlyInteger: false
+      },
+      // Specify a fixed width for the chart as a string (i.e. '100px' or '50%')
+      width: undefined,
+      // Specify a fixed height for the chart as a string (i.e. '100px' or '50%')
+      height: undefined,
+      // If the line should be drawn or not
+      showLine: true,
+      // If dots should be drawn or not
+      showPoint: true,
+      // If the line chart should draw an area
+      showArea: false,
+      // The base for the area chart that will be used to close the area shape (is normally 0)
+      areaBase: 0,
+      // Specify if the lines should be smoothed. This value can be true or false where true will result in smoothing using the default smoothing interpolation function Chartist.Interpolation.cardinal and false results in Chartist.Interpolation.none. You can also choose other smoothing / interpolation functions available in the Chartist.Interpolation module, or write your own interpolation function. Check the examples for a brief description.
+      lineSmooth: true,
+      // Overriding the natural low of the chart allows you to zoom in or limit the charts lowest displayed value
+      low: undefined,
+      // Overriding the natural high of the chart allows you to zoom in or limit the charts highest displayed value
+      high: undefined,
+      // Padding of the chart drawing area to the container element and labels as a number or padding object {top: 5, right: 5, bottom: 5, left: 5}
+      chartPadding: {
+        top: 15,
+        right: 15,
+        bottom: 5,
+        left: 10
+      },
+      // When set to true, the last grid line on the x-axis is not drawn and the chart elements will expand to the full available width of the chart. For the last label to be drawn correctly you might need to add chart padding or offset the last label with a draw event handler.
+      fullWidth: false,
+      // If true the whole data is reversed including labels, the series order as well as the whole series data arrays.
+      reverseData: false,
+      // Override the class names that get used to generate the SVG structure of the chart
+      classNames: {
+        chart: 'ct-chart-line',
+        label: 'ct-label',
+        labelGroup: 'ct-labels',
+        series: 'ct-series',
+        line: 'ct-line',
+        point: 'ct-point',
+        area: 'ct-area',
+        grid: 'ct-grid',
+        gridGroup: 'ct-grids',
+        vertical: 'ct-vertical',
+        horizontal: 'ct-horizontal',
+        start: 'ct-start',
+        end: 'ct-end'
+      }
+    };
+
+    let Chart = new Chartist.Line('.ct-chart', data, options);
+
 
     return this.autorun(computation => {
       let { property } = Template.currentData();
@@ -59,39 +132,19 @@ class LineChartComponent extends CommonComponent {
         .fetch();
       });
 
-      instance[property].paths = instance[property].svg.selectAll('path.line').data( this.datapoints() );
+      this.datapoints().map((currentValue, index, array)=> {
+        if (data.labels.length > 15) {
+          data.labels.shift();
+          data.series[0].shift();
+        }
+        data.labels.push(currentValue.insertedAt);
+        data.series[0].push(currentValue.data.value);
+      });
 
-      x.domain(d3.extent(this.datapoints(), d => d.insertedAt
-      ));
-
-      // TODO: scale things to a reasonable amount, it is weird to see it zoomed in too closely.
-      y.domain(d3.extent(this.datapoints(), d => d.data.value
-      ));
-
-      // Temperature specific HACK
-      // y.domain [0, 40]
-
-      let line = d3.svg.line().x(d => x(d.insertedAt)
-      ).y(d => y(d.data.value)
-      ).interpolate("linear");
-
-      // TODO: fix graph blinking when lots of data or when data is coming in
-      // at a different rate.
-
-      //Update X axis
-      instance[property].svg.select('.x.axis').transition().duration(1000).call(instance[property].xAxis);
-      //Update Y axis
-      instance[property].svg.select('.y.axis').transition().duration(1000).call(instance[property].yAxis);
-      instance[property].paths
-        .enter()
-        .append('path')
-        .attr('class', 'line')
-        .attr('d', line(this.datapoints()))
-        .transition()
-        .duration(1000)
-        .remove();
-      instance[property].paths.exit().remove();
-      return;
+      // Create a new line chart object where as first parameter we pass in a selector
+      // that is resolving to our chart container element. The Second parameter
+      // is the actual data object.
+      Chart.update(data, options);
     });
   }
 }
