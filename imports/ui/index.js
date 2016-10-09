c7af7189f1ae2bfcbf6e573e2ab7ef6e5e9d8e74
aruntk <aruntknambiar@gmail.com>
2016-10-09 19:39:56 +0530

    signout+account header menu

--- imports/ui/index.js
+++ imports/ui/index.js
@@ -1,5 +1,5 @@
 import './build.html';
 import './layouts/layout.js';
 
-import './components/accounts/accounts-element.html';
+import './components/accounts';
 import './components/thing/thing-display.html';
