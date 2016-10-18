bb0fb54a9a844334d937305ed290ff56dee36370
Jake Hartnell <jake.hartnell@gmail.com>
2016-10-18 12:52:17 -0700

    Clean up.

--- imports/ui/components/thing/events.js
+++
@@ -1,9 +0,0 @@
-Polymer({
-  is:"thing-events",
-  properties:{
-    events: Object
-  },
-  tracker:function(){
-    console.log(this.events);
-  }
-});
--- imports/ui/components/thing/index.js
+++ imports/ui/components/thing/index.js
@@ -1,3 +1,1 @@
-import './actions.html';
-import './events.html';
 import './thing-display.html';
