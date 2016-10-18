c30f2ee576bd46bbe3b99b66fb6639f46abdc827
Jake Hartnell <jake.hartnell@gmail.com>
2016-10-18 16:29:22 -0700

    Kill unneeded console.log()

--- test-thing.js
+++ test-thing.js
@@ -6,9 +6,9 @@ var GrowInstance = require('./dist/Grow.umd.js');
 
 // Create a new grow instance.
 var grow = new GrowInstance({
-    uuid: 'befb9fa7-6ff3-4f79-a143-6d9c43a799d8',
-    token: 'Y9LppJwc33HBM584yEwvTThmxJjvdRKn',
-    webcomponent: true, // Hack
+    uuid: 'c592bb25-fb6a-4b34-9691-bb48ee839b9e',
+    token: 'Ybdsdjsv6HHscvi4qgirsrLD7JHiL8vT',
+    webcomponent: 'test-thing', // Hack
 
     // Properties can be updated by the API
     properties: {
