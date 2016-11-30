d22dedf45b5bd2f3da48a3bf9c81b45b7820afac
Jake Hartnell <jake.hartnell@gmail.com>
2016-11-30 13:26:20 -0800

    Update test device.

--- tests/test-device.js
+++ tests/test-device.js
@@ -1,9 +1,6 @@
 // Import the latest build of the Grow.js library
 var GrowInstance = require('Grow.js');
 
-// Declare need variable for example
-var currentLightValue;
-
 // Create a new grow instance. Connects by default to localhost:3000
 var grow = new GrowInstance({
     uuid: '',
@@ -11,35 +8,28 @@ var grow = new GrowInstance({
 
     // Properties can be updated by the API
     properties: {
-        name: 'Light and photoresistor', // The display name for the thing.
-        desription: 'An LED light with a basic on/off api.',
-        state: 'off',
-        lightConditions: null,
-        light_data_interval: 3000
+        state: 'off'
     },
 
     start: function () {
-        setInterval(light_data, 3000);
+        setInterval(temp_data, 3000);
     },
 
-    // Actions are the API of the thing.
     turn_light_on: function () {
         grow.set('state', 'on');
     },
 
     turn_light_off: function () {
-        // Emit a 'light off' event, set the state property to 'off'
         grow.set('state', 'off');
     },
 
-    light_data: function () {
-        currentLightValue = Math.random();
+    temp_data: function () {
+        let temp = Math.random() * 100;
 
         // Send data to the Grow-IoT app.
-        // TODO: if a method returns a value, emit that value.
-        grow.emit({
-          type: 'light',
-          value: currentLightValue
+        grow.temp_data({
+          type: 'temperature',
+          value: temp
         });
     }
 });
