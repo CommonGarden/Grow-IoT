63c6423163d77aabd4731e8719d6621baa1ec281
Jake Hartnell <jake.hartnell@gmail.com>
2016-12-08 14:42:31 -0800

    Start cleaning up event bus.

--- imports/api/events/publish.js
+++ imports/api/events/publish.js
@@ -1,12 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
+import { Match } from 'meteor/check'
 
-// Feel free to make suggestions for the eventbus api.
-Meteor.publish('Events.byThing', function(thingUuid) {
-  check(thingUuid, String);
+Meteor.publish('Events', function(uuid, type) {
+  check(uuid, String);
+  check(type, Match.OneOf(String, undefined));
+
+  console.log(type);
 
   let thing = Thing.findOne({
-    'uuid': thingUuid,
+    'uuid': uuid,
     'owner': this.userId
   }
   , {
@@ -25,33 +28,45 @@ Meteor.publish('Events.byThing', function(thingUuid) {
     },
     'limit': 100
   });
+
+  // return Events.find({
+  //   'thing._id': thing._id,
+  //   'event.message.type': type,
+  // }
+  // , {
+  //   'sort': {
+  //     'insertedAt': -1
+  //   },
+  //   'limit': 100
+  // });
 });
 
-Meteor.publish('Events.byThingAndType', function(thingUuid, type) {
-  check(thingUuid, String);
-  check(type, String);
+// // Does this have to be a seperate function? Me thinks no.
+// Meteor.publish('Events.byThingAndType', function(thingUuid, type) {
+//   check(thingUuid, String);
+//   check(type, String);
 
-  let thing = Things.findOne(
-    {
-      'uuid': thingUuid,
-      'owner': this.userId
-    }
-  , {
-    fields: {
-      _id: 1
-    }
-  });
+//   let thing = Things.findOne(
+//     {
+//       'uuid': thingUuid,
+//       'owner': this.userId
+//     }
+//   , {
+//     fields: {
+//       _id: 1
+//     }
+//   });
 
-  if (!thing) { throw new Meteor.Error('not-found', `Thing '${thingUuid}' cannot be found.`); }
+//   if (!thing) { throw new Meteor.Error('not-found', `Thing '${thingUuid}' cannot be found.`); }
 
-  return Events.find({
-    'thing._id': thing._id,
-    'event.message.type': type,
-  }
-  , {
-    'sort': {
-      'insertedAt': -1
-    },
-    'limit': 100
-  });
-});
+//   return Events.find({
+//     'thing._id': thing._id,
+//     'event.message.type': type,
+//   }
+//   , {
+//     'sort': {
+//       'insertedAt': -1
+//     },
+//     'limit': 100
+//   });
+// });
