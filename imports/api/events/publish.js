d88de5205ffdcfb7e163445780b5ec75f0717ed4
Jake Hartnell <jake.hartnell@gmail.com>
2016-12-09 14:00:27 -0800

    Clean up events.

--- imports/api/events/publish.js
+++ imports/api/events/publish.js
@@ -2,13 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 import { Match } from 'meteor/check'
 
-Meteor.publish('Events', function(uuid, type) {
+Meteor.publish('Thing.events', function(uuid, type) {
   check(uuid, String);
   check(type, Match.OneOf(String, undefined));
 
-  console.log(type);
-
-  let thing = Thing.findOne({
+  let thing = Things.findOne({
     'uuid': uuid,
     'owner': this.userId
   }
@@ -20,53 +18,27 @@ Meteor.publish('Events', function(uuid, type) {
 
   if (!thing) { throw new Meteor.Error('not-found', `Thing '${thingUuid}' cannot be found.`); }
 
-  return Events.find(
-    {'thing._id': thing._id}
-  , {
-    'sort': {
-      'insertedAt': -1
-    },
-    'limit': 100
-  });
+  if (type === undefined) {
+      return Events.find(
+        {'thing._id': thing._id}
+      , {
+        'sort': {
+          'insertedAt': -1
+        },
+        'limit': 100
+      });
+  }
 
-  // return Events.find({
-  //   'thing._id': thing._id,
-  //   'event.message.type': type,
-  // }
-  // , {
-  //   'sort': {
-  //     'insertedAt': -1
-  //   },
-  //   'limit': 100
-  // });
+  if (type) {
+    return Events.find({
+      'thing._id': thing._id,
+      'event.type': type,
+    }
+    , {
+      'sort': {
+        'insertedAt': -1
+      },
+      'limit': 100
+    });
+  }
 });
-
-// // Does this have to be a seperate function? Me thinks no.
-// Meteor.publish('Events.byThingAndType', function(thingUuid, type) {
-//   check(thingUuid, String);
-//   check(type, String);
-
-//   let thing = Things.findOne(
-//     {
-//       'uuid': thingUuid,
-//       'owner': this.userId
-//     }
-//   , {
-//     fields: {
-//       _id: 1
-//     }
-//   });
-
-//   if (!thing) { throw new Meteor.Error('not-found', `Thing '${thingUuid}' cannot be found.`); }
-
-//   return Events.find({
-//     'thing._id': thing._id,
-//     'event.message.type': type,
-//   }
-//   , {
-//     'sort': {
-//       'insertedAt': -1
-//     },
-//     'limit': 100
-//   });
-// });
