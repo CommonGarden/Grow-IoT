9813c61bfadf9afa4e59aef7b2b4529685c6ef1a
aruntk <aruntknambiar@gmail.com>
2016-12-09 23:33:53 +0530

    bower.sh to execute bower commands. example: ./bower.sh install --save PolymerElements/app-route

---
+++ bower.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Clean bower install.
+
+if [ $# -ne 0 ]; then
+  if bower $@; then
+    echo "successful"
+  else
+    echo "command failed"
+    exit 1
+  fi
+  read -p "Remove demo,test folders & files from bower dir. (y/n)?" CONT
+  if [ "$CONT" = "y" ]; then
+    folders=( "demo" "test" "bower_components" )
+    for i in "${folders[@]}"
+    do
+      find ./imports/ui/bower_components/* -depth -name $i -exec rm -rf "{}" \;
+    done
+    files=( "demo" "test" )
+    for i in "${files[@]}"
+    do
+      find ./imports/ui/bower_components/* -depth -name "${i}.*" -type f|xargs rm -f
+    done
+    echo "Cleanup Successful.!";
+  else
+    echo "Cleanup Cancelled";
+  fi
+else
+  echo "no arguments supplied"
+fi
