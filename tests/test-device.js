576f8ee2bce2f4023adafbc264cfb1da0766c6a0
aruntk <aruntknambiar@gmail.com>
2016-12-10 16:33:33 +0530

    test device selects elements according to event type. grow elements demand the values they need from test thing.

--- tests/test-device.js
+++ tests/test-device.js
@@ -2,8 +2,8 @@ var Thing = require('Grow.js');
 
 var testDevice = new Thing({
   // PUT YOUR UUID AND TOKEN HERE: |||||||||||||||
-  uuid: 'Paste_UUID_here',
-  token: 'Paste_TOKEN_here',
+  uuid: '6e42b6d8-1c3b-4a69-9527-4b8d20f8486c',
+  token: 'nLoyuaKpuMG4nCZTYfuRHQtoMFKPbb2j',
 
   testDevice: true, // HACK, unfortunately needed for now...
   component: 'test-device', // The future...
