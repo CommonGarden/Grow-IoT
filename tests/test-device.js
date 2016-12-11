c9f77fdbd0e6958c427952a9cb40994c02dc3d4c
aruntk <aruntknambiar@gmail.com>
2016-12-11 06:38:33 +0530

    uuid+token as arguments + type inquiry

--- tests/test-device.js
+++ tests/test-device.js
@@ -1,42 +1,57 @@
 var Thing = require('Grow.js');
-
-var testDevice = new Thing({
-  // PUT YOUR UUID AND TOKEN HERE
-  uuid: 'PASTE_UUID_HERE',
-  token: 'PASTE_TOKEN_HERE',
-
-  component: 'test-device',
-
-  properties: {
-    state: 'off'
+var inquirer = require('inquirer');
+var args = process.argv.slice(2);
+var uuid = args[0];
+var token = args[1];
+var questions = [
+  {
+    type: 'list',
+    name: 'type',
+    message: 'Select Event Type',
+    choices: ['temperature', 'ph', 'humidity']
   },
+]
+inquirer.prompt(questions).then(function (answers) {
+  const type = answers.type || 'temperature';
+  console.log(`\nCreating ${type} events.`);
+  var testDevice = new Thing({
+    // PUT YOUR UUID AND TOKEN HERE
+    uuid: uuid,
+    token: token,
 
-  start: function () {
-    setInterval(()=> {
-      testDevice.call('temp_data');
-    }, 3000);
-  },
+    component: 'test-device',
 
-  turn_on: function () {
-    console.log('on');
-    testDevice.set('state', 'on');
-  },
+    properties: {
+      state: 'off'
+    },
 
-  turn_off: function () {
-    console.log('off');
-    testDevice.set('state', 'off');
-  },
+    start: function () {
+      setInterval(()=> {
+        testDevice.call('temp_data');
+      }, 3000);
+    },
 
-  temp_data: function () {
-    let temp = Math.random() * 100;
+    turn_on: function () {
+      console.log('on');
+      testDevice.set('state', 'on');
+    },
 
-    console.log(temp);
+    turn_off: function () {
+      console.log('off');
+      testDevice.set('state', 'off');
+    },
 
-    testDevice.emit({
-      type: 'temperature',
-      value: temp
-    });
-  }
+    temp_data: function () {
+      let temp = Math.random() * 100;
+
+      console.log(temp);
+
+      testDevice.emit({
+        type: type,
+        value: temp
+      });
+    }
+  });
+  testDevice.connect();
+
 });
-
-testDevice.connect();
