64573f0059d8bc59f81153b5a343d287d3653ec3
Jake Hartnell <jake.hartnell@gmail.com>
2016-12-13 21:10:08 -0800

    thing-toolbar

--- client/main.js
+++ client/main.js
@@ -1,4 +1,4 @@
+import '../imports/ui/index.js';
 import '../imports/collections/index.js';
 import '../imports/api/index.js';
-import '../imports/ui/index.js';
 import '../imports/examples/index.js';
