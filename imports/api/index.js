cb23741a65ddf0b772240ca5b446f648e75edc7c
aruntk <aruntknambiar@gmail.com>
2016-12-26 11:21:30 +0530

    minor

--- imports/api/index.js
+++ imports/api/index.js
@@ -1,9 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 
 if (Meteor.isServer) {
-	import './server-methods.js';
-	import './publish.js';
+  import './server-methods.js';
+  import './publish.js';
+  import './elastic';
 }
 
 import './methods.js';
-import './elastic';
