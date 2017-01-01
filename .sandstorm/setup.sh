3f10818a3fa861127f113711099914c7157c7e47
aruntk <aruntknambiar@gmail.com>
2017-01-01 07:25:28 +0530

    sandstorm working now

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
+set -x
+set -euvo pipefail
 
-# When you change this file, you must take manual action. Read this doc:
-# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
-
-set -euo pipefail
-
-CURL_OPTS="--silent --show-error"
+apt-get update
+apt-get install build-essential git -y
 
 cd /opt/
 
-PACKAGE=meteor-spk-0.1.8
+NODE_ENV=production
+PACKAGE=meteor-spk-0.3.1
 PACKAGE_FILENAME="$PACKAGE.tar.xz"
 CACHE_TARGET="/host-dot-sandstorm/caches/${PACKAGE_FILENAME}"
 
 # Fetch meteor-spk tarball if not cached
 if [ ! -f "$CACHE_TARGET" ] ; then
-    echo -n "Downloading ${PACKAGE}..."
-    curl $CURL_OPTS https://dl.sandstorm.io/${PACKAGE_FILENAME} > "$CACHE_TARGET.partial"
-    mv "${CACHE_TARGET}.partial" "${CACHE_TARGET}"
-    echo "...done."
+    curl https://dl.sandstorm.io/${PACKAGE_FILENAME} > "$CACHE_TARGET"
 fi
 
 # Extract to /opt
 tar xf "$CACHE_TARGET"
 
 # Create symlink so we can rely on the path /opt/meteor-spk
-if [ ! -e meteor-spk ] ; then ln -s "${PACKAGE}" meteor-spk ; fi
+ln -s "${PACKAGE}" meteor-spk
 
 # Add bash, and its dependencies, so they get mapped into the image.
 # Bash runs the launcher script.
@@ -36,7 +32,7 @@ cp -a /lib/x86_64-linux-gnu/libtinfo.so.* /opt/meteor-spk/meteor-spk.deps/lib/x8
 # Unfortunately, Meteor does not explicitly make it easy to cache packages, but
 # we know experimentally that the package is mostly directly extractable to a
 # user's $HOME/.meteor directory.
-METEOR_RELEASE=1.1.0.2
+METEOR_RELEASE=1.4.2
 METEOR_PLATFORM=os.linux.x86_64
 METEOR_TARBALL_FILENAME="meteor-bootstrap-${METEOR_PLATFORM}.tar.gz"
 METEOR_TARBALL_URL="https://d3sqy0vbqsdhku.cloudfront.net/packages-bootstrap/${METEOR_RELEASE}/${METEOR_TARBALL_FILENAME}"
@@ -44,14 +40,13 @@ METEOR_CACHE_TARGET="/host-dot-sandstorm/caches/${METEOR_TARBALL_FILENAME}"
 
 # Fetch meteor tarball if not cached
 if [ ! -f "$METEOR_CACHE_TARGET" ] ; then
-    echo -n "Downloading Meteor version ${METEOR_RELEASE}..."
-    curl $CURL_OPTS "$METEOR_TARBALL_URL" > "${METEOR_CACHE_TARGET}.partial"
+    curl "$METEOR_TARBALL_URL" > "${METEOR_CACHE_TARGET}.partial"
     mv "${METEOR_CACHE_TARGET}"{.partial,}
-    echo "...done."
 fi
 
 # Extract as unprivileged user, which is the usual meteor setup
 cd /home/vagrant/
 su -c "tar xf '${METEOR_CACHE_TARGET}'" vagrant
 # Link into global PATH
-if [ ! -e /usr/bin/meteor ] ; then ln -s /home/vagrant/.meteor/meteor /usr/bin/meteor ; fi
+ln -s /home/vagrant/.meteor/meteor /usr/bin/meteor
+chown vagrant:vagrant /home/vagrant -R
