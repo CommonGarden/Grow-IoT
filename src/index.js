d843cf25100a75318552a2c2e5f2a658a0c57507
aruntk <aruntknambiar@gmail.com>
2017-02-10 12:22:56 +0530

    password export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Events from './events';
 import Things from './things';
 import User from './user';
+import Password from './auth/password';
 
-export default { Events, Things, User };
+export default { Events, Things, User, Password };
