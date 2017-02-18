c9c70f08e80a5e0812986a01ddb03476ec8d6bad
Jake Hartnell <jake.hartnell@gmail.com>
2017-02-18 14:59:23 -0800

    Add .codeclimate.yml

--- examples/multiple-things.js
+++ examples/multiple-things.js
@@ -18,7 +18,7 @@ thing1.call('method');
 // Event emitter api is also available.
 thing1.on('property-updated', function(key) {
   let addition = thing2.adder.call('add', [10, 2, 3]);
-  thing2.call('echo', 'Property ' + key + ' updated. Addition result: ' + addition);
+  thing2.call('echo', 'Property ' + key + ' updated. Addition result: ' +  addition);
   thing1.call('method');
 });
 
