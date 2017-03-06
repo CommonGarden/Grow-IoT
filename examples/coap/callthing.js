878a6a9f0a3e798648ec43dbde3dc24b831705a7
Jake Hartnell <Jake.Hartnell@gmail.com>
2017-03-05 22:26:01 -0800

    Silly coap example.

--- examples/coap/callthing.js
+++ examples/coap/callthing.js
@@ -7,3 +7,19 @@ req.on('response', function(res) {
 })
 
 req.end()
+
+var req2 = coap.request('coap://localhost/get?key=state');
+
+req2.on('response', function(res2) {
+  res2.pipe(process.stdout)
+});
+
+req2.end();
+
+// var req3 = coap.request('coap://localhost/set?key=state&value=true');
+
+// req3.on('response', function(res3) {
+//   res3.pipe(process.stdout)
+// })
+
+// req3.end()
