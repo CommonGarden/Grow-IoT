cf117e2b61182e19a09a2083ac22fec9397797e2
aruntk <aruntknambiar@gmail.com>
2017-03-12 11:48:48 +0530

    ThingsList and ThingDisplay

---
+++ imports/ui/pages/ThingsList.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+import ThingDisplay from '../components/ThingDisplay.jsx';
+
+class ThingsList extends Component {
+  componentWillMount() {
+    this.subThings();
+  }
+  subThings(){
+    Meteor.subscribe('Things.list');
+  }
+  render () {
+    const thingStyle = {
+      margin: '20px',
+    }
+    return (
+      <div className="layout horizontal wrap">
+        {
+          this.props.Things.map((v, k) => {
+            return (
+              <ThingDisplay key={k}
+                thing={v}
+                className="thing-card"
+                style={thingStyle}/>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default ThingsListContainer= createContainer(({ user }) => {
+  return {
+    Things: Things.find().fetch(),
+  }
+}, ThingsList);
