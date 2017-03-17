a299a94813dedfcd7f1d70b2941055eace1a0510
Jake Hartnell <Jake.Hartnell@gmail.com>
2017-03-17 16:40:42 -0700

    Update smart light example.

--- examples/arduino/smart-light/smart-light.js
+++ examples/arduino/smart-light/smart-light.js
@@ -21,99 +21,102 @@ board.on('ready', function start() {
 
   // Create a new thing.
   var light = new Thing({
-    uuid: '',
-    token: '',
+    uuid: '639cb174-4b59-4923-992c-f6fb5571998a',
+    token: 'hXAYxPEvhRk3TMq5BteFgDPYShAEDrrw',
 
     component: 'smart-light',
 
     properties: {
-        state: 'off',
-        threshold: 300,
-        interval: 3000,
-        currently: null,
-        lightconditions: null,
-        cycles: {
-            day: {
-                start: 'after 7:00am'
-            },
-            night: {
-                start: 'after 8:00pm'
-            }
+      state: 'off',
+      threshold: 300,
+      interval: 3000,
+      currently: null,
+      lightconditions: null,
+      cycles: {
+        day: {
+          start: 'after 7:00am'
+        },
+        night: {
+          start: 'after 8:00pm'
         }
+      }
     },
 
     start: function () {
-        var interval = this.get('interval');
-        
-        this.interval = setInterval(function () {
-            light.light_data();
-            light.check_light_data();
-        }, interval);
+      console.log(this.properties)
+      var interval = this.get('interval');
+      
+      this.interval = setInterval(function () {
+        light.light_data();
+        light.check_light_data();
+      }, interval);
 
-        this.parseCycles(this.get('cycles'));
+      this.parseCycles(this.get('cycles'));
     },
 
     stop: function () {
-        clearInterval(this.interval);
-        this.removeAllListeners();
+      clearInterval(this.interval);
+      this.removeAllListeners();
     },
 
     day: function () {
-        console.log('It is day!');
-        this.set('currently', 'day');
-        this.call('turn_on');
+      console.log('It is day!');
+      this.set('currently', 'day');
+      this.call('turn_on');
     },
 
     night: function () {
-        console.log('It is night!');
-        this.set('currently', 'night');
-        this.call('turn_off');
+      console.log('It is night!');
+      this.set('currently', 'night');
+      this.call('turn_off');
     },
 
     turn_on: function () {
-        LED.high();
-        this.set('state', 'on');
-        console.log('light on');
+      LED.high();
+      this.set('state', 'on');
+      console.log('light on');
     },
 
     turn_off:  function () {
-        LED.low();
-        this.set('state', 'off');
-        console.log('light off')
+      LED.low();
+      this.set('state', 'off');
+      console.log('light off');
     },
 
     light_data: function () {
-        console.log(lightSensor.value);
+      console.log(lightSensor.value);
 
-        light.emit({
-          type: 'light',
-          value: lightSensor.value
-        });
+      light.emit({
+        type: 'light',
+        value: lightSensor.value
+      });
     },
 
     check_light_data: function () {
-        let threshold = this.get('threshold');
-        let state = this.get('state');
-        let currently = this.get('currently');
+      let threshold = this.get('threshold');
+      let state = this.get('state');
+      let currently = this.get('currently');
 
-        if ((lightSensor.value < threshold) && 
-            (this.get('lightconditions') != 'dark') &&
-            (currently === 'day')) {
-            console.log('Too dark for daylight hours, turning on light.')
-            this.set('lightconditions', 'dark');
-            this.call('turn_on');
-        } 
+      if ((lightSensor.value < threshold) &&
+        (this.get('lightconditions') != 'dark') &&
+        (currently === 'day')) {
 
-        else if ((lightSensor.value >= threshold) && 
-            (this.get('lightconditions') != 'light') &&
-            (currently === 'day')) {
-            this.set('lightconditions', 'light');
-            this.call('turn_off');
-        }
+        console.log('Too dark for daylight hours, turning on light.')
+        this.set('lightconditions', 'dark');
+        this.call('turn_on');
+      }
+
+      else if ((lightSensor.value >= threshold) &&
+        (this.get('lightconditions') != 'light') &&
+        (currently === 'day')) {
+
+        this.set('lightconditions', 'light');
+        this.call('turn_off');
+      }
     }
   });
 
-  // light.connect(
+  light.connect(
   // {
   //     host: "grow.commongarden.org",
   //     tlsOpts: {
@@ -124,5 +127,5 @@ board.on('ready', function start() {
   //     port: 443,
   //     ssl: true
   // }
-  // );
+  );
 });
