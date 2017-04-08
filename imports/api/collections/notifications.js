e11126e8dd144dc30977e4df2bb44e2acd7805fd
aruntk <aruntknambiar@gmail.com>
2017-04-08 14:11:57 +0530

    notifications collections

---
+++ imports/api/collections/notifications.js
@@ -0,0 +1,32 @@
+import { Mongo } from 'meteor/mongo';
+
+const Notifications = new Mongo.Collection('Notifications');
+
+Notifications.allow({
+  insert: function (userId, doc) {
+    // the user must be logged in, and the document must be owned by the user
+    return (userId && doc.owner === userId);
+  },
+  update: function (userId, doc, fields, modifier) {
+    // can only change your own documents
+    return doc.owner === userId;
+  },
+  remove: function (userId, doc) {
+    // can only remove your own documents
+    return doc.owner === userId;
+  },
+  fetch: ['owner']
+});
+Notifications.deny({
+  update: function (userId, doc, fields, modifier) {
+    // can't change owners
+    return _.contains(fields, 'owner');
+  },
+  remove: function (userId, doc) {
+    // can't remove locked documents
+    return doc.locked;
+  },
+  fetch: ['locked'] // no need to fetch 'owner'
+});
+
+export default Notifications;
