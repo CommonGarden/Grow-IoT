839b39d0f75c36041d662825cb25b3863ff9b5bb
aruntk <aruntknambiar@gmail.com>
2017-08-14 10:45:21 +0530

    prevent eventbus on startup #385

--- imports/api/events/eventBus.js
+++ imports/api/events/eventBus.js
@@ -5,66 +5,71 @@ export const client = monq(MONGO_URL);
 export const queue = client.queue('eventsqueue');
 
 // A work in progress...
-
-const enqueueTask = function (id, type, collection, index) {
-  const task = {
-    id,
-    type,
-    collection,
-    index,
+Meteor.startup(() => {
+  let initialized = false;
+  const enqueueTask = function (id, type, collection, index) {
+    if (initialized) {
+      const task = {
+        id,
+        type,
+        collection,
+        index,
+      };
+      // If type == alert, send notification?
+      queue.enqueue('events', task, (err, job) => {
+        if (err) {throw err}
+      });
+    }
   };
-  // If type == alert, send notification?
-  queue.enqueue('events', task, (err, job) => {
-    if (err) {throw err}
+
+  const EventBus = (collection, index) => {
+    const cursor = collection.find({});
+    cursor.observeChanges({
+      added: function (id) {
+        enqueueTask(id, 'added', collection._name, index);
+      },
+      changed: function (id) {
+        enqueueTask(id, 'changed', collection._name, index);
+      },
+      removed: function (id) {
+        enqueueTask(id, 'removed', collection._name, index);
+      },
+    });
+  };
+
+  EventBus(Events, 'event');
+
+  const worker = client.worker(['eventsqueue']);
+
+  worker.register({
+    events: function (params, callback) {
+      try {
+        switch(params.type) {
+          case 'added':
+            added(params.id, params.collection, params.index);
+            break;
+          case 'removed': 
+            removed(params.id, params.collection, params.index);
+            break;
+          default:
+            break;
+        }
+        callback(null, params);
+      } catch (err) {
+        callback(err);
+      }
+    }
   });
-};
 
-const EventBus = (collection, index) => {
-  const cursor = collection.find({});
-  cursor.observeChanges({
-    added: function (id) {
-      enqueueTask(id, 'added', collection._name, index);
-    },
-    changed: function (id) {
-      enqueueTask(id, 'changed', collection._name, index);
-    },
-    removed: function (id) {
-      enqueueTask(id, 'removed', collection._name, index);
-    },
-  });
-};
+  worker.start();
 
-EventBus(Events, 'event');
+  function added(id, collection, index) {
+    // console.log('added')
+    // query MongoDB for the document
+  }
 
-const worker = client.worker(['eventsqueue']);
-
-worker.register({
-	events: function (params, callback) {
-	  try {
-	    switch(params.type) {
-			  case 'added':
-			    added(params.id, params.collection, params.index);
-			    break;
-			  case 'removed': 
-			    removed(params.id, params.collection, params.index);
-			    break;
-			  default:
-			    break;
-			}
-	    callback(null, params);
-	  } catch (err) {
-	    callback(err);
-	  }
-	}
+  function removed(id, collection, index) {
+    //
+  }
+  initialized = true;
 });
-
-worker.start();
-
-function added(id, collection, index) {
-	// console.log('added')
-  // query MongoDB for the document
-}
-
-function removed(id, collection, index) {
-	//
-}
