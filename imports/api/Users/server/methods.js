8f1f30988ac603fe0d764ab272494f89e4b7a136
Dan Minshew <ofenixculpa@gmail.com>
2018-01-13 18:04:56 -0600

    Update text of 2-clause BSD license

---
+++ imports/api/Users/server/methods.js
@@ -0,0 +1,37 @@
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+import { Accounts } from 'meteor/accounts-base';
+import editProfile from './edit-profile';
+// import rateLimit from '../../../modules/rate-limit';
+
+Meteor.methods({
+  'users.sendVerificationEmail': function usersSendVerificationEmail() {
+    return Accounts.sendVerificationEmail(this.userId);
+  },
+  'users.editProfile': function usersEditProfile(profile) {
+    check(profile, {
+      emailAddress: String,
+      profile: {
+        name: {
+          first: String,
+          last: String,
+        },
+      },
+    });
+
+    return editProfile({ userId: this.userId, profile })
+      .then(response => response)
+      .catch((exception) => {
+        throw new Meteor.Error('500', exception);
+      });
+  },
+});
+
+/*rateLimit({
+  methods: [
+    'users.sendVerificationEmail',
+    'users.editProfile',
+  ],
+  limit: 5,
+  timeRange: 1000,
+});*/
---
+++ imports/api/Users/server/publications.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.publish('users.editProfile', function usersProfile() {
+  return Meteor.users.find(this.userId, {
+    fields: {
+      emails: 1,
+      profile: 1,
+      services: 1,
+    },
+  });
+});
